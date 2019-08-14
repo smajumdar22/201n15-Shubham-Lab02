a8d0f972b77fdd1298285c2619b943d3f335e6d9
Shubham Majumdar <smajumdar22@gmail.com>
2019-08-13 23:32:43 -0700

    Modify javascript and add comments

--- js/app.js
+++ js/app.js
@@ -1,15 +1,25 @@
 'use strict';
-var userName = prompt('What is your name?');
-console.log('user name -' + userName);
+
+var userName = prompt('What is your name?');//defining variable and assigning value to variable
+console.log('user name -' + userName);//adding console log
+//Displaying message
 alert('Hello '+ userName + '.Lets play a game called GUESS WHAT?Are you ready');
 
+//defining variable and assigning value to variable
 var genreMovies = prompt('Do you think I like horror movies?');
+//adding console log
 console.log('Answer genre movies - ' + genreMovies);
+//changing user answer to lower case
 genreMovies = genreMovies.toLowerCase();
+//initializing counter for score
 var count = 0;
+//validating user response to incremnet score
 if(genreMovies === 'yes'||genreMovies === 'no'|| genreMovies === 'y'|| genreMovies === 'n'){
   alert('ok lets move on ' + userName);
-  count ++;
+  if(genreMovies === 'yes'||genreMovies === 'y'){
+    //incrementing score
+    count++;
+  }
 
 }
 else{
@@ -21,6 +31,9 @@ console.log('Answer fav food -' + favFood);
 favFood = favFood.toLowerCase();
 if(favFood === 'yes'||favFood === 'no'|| favFood === 'y'|| favFood === 'n'){
   alert('ok lets move on ' + userName);
+  if(favFood === 'yes'||favFood === 'y'){
+    count++;
+  }
 }
 else {
   alert(userName + ' You are one stubborn human being.');
@@ -31,6 +44,9 @@ console.log('Answer fav drink -' + favDrink);
 favDrink = favDrink.toLowerCase();
 if(favDrink=== 'yes'||favFood === 'no'|| favFood === 'y'|| favFood === 'n'){
   alert('ok lets move on ' + userName);
+  if(favDrink=== 'yes'||favFood === 'y'){
+    count++;
+  }
 }
 else{
   alert(userName + ' I dont know what to do with you');
@@ -41,6 +57,9 @@ console.log('Answer fav pet -' + favPet);
 favPet = favPet.toLowerCase();
 if(favPet=== 'yes'||favPet=== 'no'|| favPet === 'y'|| favPet === 'n'){
   alert('ok lets move on ' + userName);
+  if(favPet=== 'yes'||favPet=== 'y'){
+    count++
+  }
 }
 else{
   alert(userName + ' I think you simply cant read');
@@ -51,8 +70,20 @@ console.log('Answer fav place -' + favPlace);
 favPlace = favPlace.toLowerCase();
 if(favPlace=== 'yes'||favPlace=== 'no'|| favPlace === 'y'|| favPlace === 'n'){
   alert('ok lets move on ' + userName);
+  if(favPlace=== 'yes'||favPlace=== 'y'){
+    count++
+  }
 }
 else{
   alert(userName + ' I have no hope for you');
+}
+alert(userName + ' your total score is '+ count +'/5');
+
+//Checking count value to display message
+if(count >=3){
+  alert(userName + ' you passed')
+}
+else{
+  alert(userName + ' you failed')
 }
 
